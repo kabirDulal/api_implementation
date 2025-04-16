6c7ce8021f46ccd7a77042f28a5e20896f818907
Kabir Dulal <zamfy_fade21@yahoo.com>
2025-04-16 11:58:05 +1000

    added Image api to display image in grid view with title and description

--- lib/models/pictures_model.dart
+++ lib/models/pictures_model.dart
@@ -1,28 +1,32 @@
 
 class PicturesModel {
-  int? albumId;
-  int? id;
-  String? title;
-  String? url;
-  String? thumbnailUrl;
+  bool? success;
+  int? totalPhotos;
+  String? message;
+  int? offset;
+  int? limit;
+  List<Photos>? photos;
 
-  PicturesModel({this.albumId, this.id, this.title, this.url, this.thumbnailUrl});
+  PicturesModel({this.success, this.totalPhotos, this.message, this.offset, this.limit, this.photos});
 
   PicturesModel.fromJson(Map<String, dynamic> json) {
-    if(json["albumId"] is int) {
-      albumId = json["albumId"];
+    if(json["success"] is bool) {
+      success = json["success"];
     }
-    if(json["id"] is int) {
-      id = json["id"];
+    if(json["total_photos"] is int) {
+      totalPhotos = json["total_photos"];
     }
-    if(json["title"] is String) {
-      title = json["title"];
+    if(json["message"] is String) {
+      message = json["message"];
     }
-    if(json["url"] is String) {
-      url = json["url"];
+    if(json["offset"] is int) {
+      offset = json["offset"];
     }
-    if(json["thumbnailUrl"] is String) {
-      thumbnailUrl = json["thumbnailUrl"];
+    if(json["limit"] is int) {
+      limit = json["limit"];
+    }
+    if(json["photos"] is List) {
+      photos = json["photos"] == null ? null : (json["photos"] as List).map((e) => Photos.fromJson(e)).toList();
     }
   }
 
@@ -32,11 +36,56 @@ class PicturesModel {
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> _data = <String, dynamic>{};
-    _data["albumId"] = albumId;
-    _data["id"] = id;
-    _data["title"] = title;
+    _data["success"] = success;
+    _data["total_photos"] = totalPhotos;
+    _data["message"] = message;
+    _data["offset"] = offset;
+    _data["limit"] = limit;
+    if(photos != null) {
+      _data["photos"] = photos?.map((e) => e.toJson()).toList();
+    }
+    return _data;
+  }
+}
+
+class Photos {
+  String? url;
+  String? title;
+  int? user;
+  String? description;
+  int? id;
+
+  Photos({this.url, this.title, this.user, this.description, this.id});
+
+  Photos.fromJson(Map<String, dynamic> json) {
+    if(json["url"] is String) {
+      url = json["url"];
+    }
+    if(json["title"] is String) {
+      title = json["title"];
+    }
+    if(json["user"] is int) {
+      user = json["user"];
+    }
+    if(json["description"] is String) {
+      description = json["description"];
+    }
+    if(json["id"] is int) {
+      id = json["id"];
+    }
+  }
+
+  static List<Photos> fromList(List<Map<String, dynamic>> list) {
+    return list.map(Photos.fromJson).toList();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> _data = <String, dynamic>{};
     _data["url"] = url;
-    _data["thumbnailUrl"] = thumbnailUrl;
+    _data["title"] = title;
+    _data["user"] = user;
+    _data["description"] = description;
+    _data["id"] = id;
     return _data;
   }
 }
